d71900389ec73e06be0984361d0abb499c847254
imronpuji <imronpuji5@gmail.com>
2020-09-07 20:31:57 +0700

    add static configuration

--- src/api/axios/axios.js
+++ src/api/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const token = localStorage.getItem('token')
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-export default  aaxios.create({
+export default  axios.create({
     baseURL: 'https://apisiakad.hilmimubarok.com',
     /* other custom settings */
   });
