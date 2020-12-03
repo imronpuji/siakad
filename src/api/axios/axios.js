8bcd71706a10903f95240e031475957a3d7c7975
imronpuji <imronpuji5@gmail.com>
2020-12-03 19:52:40 +0700

    'latest-v1'

--- src/api/axios/axios.js
+++ src/api/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 export default axios.create({
-    baseURL: 'https://apisiakad.hilmimubarok.com'
+    baseURL: 'https://api.siakad-filkomuniss.com'
     // baseURL: 'https://dacc9b6c47ac.ngrok.io'
  
   })
